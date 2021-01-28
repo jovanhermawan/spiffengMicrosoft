c34f8a2564f2bad8d6a2e5e5d4770a7d812f541e
cutedoggies <65653917+cutedoggies@users.noreply.github.com>
2021-01-29 02:29:34 +0800

    Add files via upload

---
+++ feed.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    title: 'One spoon of this vegetable fulfils your daily protein requirement',
+    image: 'https://images.pexels.com/photos/6061856/pexels-photo-6061856.jpeg?auto=compress&cs=tinysrgb&auto=format&fit=crop&w=2100&q=80',
+    cta: 'View article',
+    horizontal: true
+  },
+  {
+    title: 'The best way to cook Spinach...',
+    image: 'https://images.pexels.com/photos/3629537/pexels-photo-3629537.jpeg?auto=compress&cs=tinysrgb&auto=format&fit=crop&w=2004&q=80',
+    cta: 'View article'
+  },
+  {
+    title: 'Are you getting enough nutrients from...',
+    image: 'https://images.pexels.com/photos/4117545/pexels-photo-4117545.jpeg?auto=compress&cs=tinysrgb&dpr=2&auto=format&fit=crop&w=2102&q=80',
+    cta: 'View article'
+  },
+];
