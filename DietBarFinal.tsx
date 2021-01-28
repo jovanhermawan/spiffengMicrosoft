2e7b547fa17c1d419054e65ddef5fee5c05faedf
darisdzakwanhoesien2 <48860799+darisdzakwanhoesien2@users.noreply.github.com>
2021-01-28 20:37:30 +0800

    Add files via upload
    Adding the statistic part on diet, expenses and progress bar. Look for the UI Design as references: https://www.figma.com/file/eGQKlxJckxMvIYpuxxGNXa/Spiffeng-UI

---
+++ DietBarFinal.tsx
@@ -0,0 +1,146 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View, TouchableHighlight, Dimensions } from 'react-native';
+
+export default function DietBarFinal() {
+  return (
+  	<View style={{backgroundColor:'#F9F9FB', flex:1}}>
+	    <View style={{top: 125, alignItems:'center', justifyContent: 'center'}}>
+        <Text style={{fontSize:22, fontWeight: 'bold'}}>
+          Insights
+        </Text>
+      </View> 
+	    <View style={{top: 150, alignItems:'center', justifyContent: 'center'}}>
+          <ScrollView style={{flex: 1}}>
+            <View style={styles.container}>
+              <StatusBar
+                hidden={true}
+              />
+              <Text style={styles.title}>Basic</Text>
+              <PieChart
+                chart_wh={chart_wh}
+                series={series}
+                sliceColor={sliceColor}
+              />
+              <Text style={styles.title}>Doughnut</Text>
+              <PieChart
+                chart_wh={chart_wh}
+                series={series}
+                sliceColor={sliceColor}
+                doughnut={true}
+                coverRadius={0.45}
+                coverFill={'#FFF'}
+              />
+            </View>
+          </ScrollView>
+      </View>
+      <View style={{top:175, left:40}}>
+        <Text style={{fontSize:18, fontWeight:'bold', color:'#1C1939'}}>
+          Expenses
+        </Text>
+      </View>
+      <View style={{top:200, left:40, flex:1, flexDirection:'row', justifyContent:'flex-start'}}>
+      <View>
+          <Image style={{width:41, height:46, borderRadius:16}} source={require('./water.png')}></Image>
+          </View>
+        <View style={{left:20}}>
+          <Text style={{fontWeight:'bold'}}>
+            Protein
+          </Text>
+          <Text>
+            80% of Recommended Weekly Intake
+          </Text>
+        </View>
+        <SegmentedProgressBar
+          showSeparatorValue
+          borderRadius={3}
+          values={[0, 80, 100]}
+          colors={['green','grey']}
+        />
+      </View>
+      <View style={{top:125, left:40, flex:1, flexDirection:'row', justifyContent:'flex-start'}}>
+        <View>
+          <Image style={{width:41, height:46, borderRadius:16}} source={require('./harvest.png')}></Image>
+          </View>
+        <View style={{left:20}}>
+          <Text style={{fontWeight:'bold'}}>
+            Iron
+          </Text>
+          <Text>
+            60% of Recommended Weekly Intake
+          </Text>
+        </View>
+        <SegmentedProgressBar
+          showSeparatorValue
+          borderRadius={3}
+          values={[0, 60, 100]}
+          colors={['green','grey']}
+        />
+      </View>
+      <View style={{top:50, left:40, flex:1, flexDirection:'row', justifyContent:'flex-start'}}>
+      <View>
+          <Image style={{width:41, height:46, borderRadius:16}} source={require('./trade.png')}></Image>
+          </View>
+        <View style={{left:20}}>
+          <Text style={{fontWeight:'bold'}}>
+            Calcium
+          </Text>
+          <Text>
+            30% of Recommended Weekly Intake
+          </Text>
+        </View>
+        <SegmentedProgressBar
+          showSeparatorValue
+          borderRadius={3}
+          values={[0, 30, 100]}
+          colors={['green','grey']}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  left:{
+  	position:'absolute',
+  	left: 17
+  },
+  right:{
+  	position:'absolute',
+  	right: 14
+  },
+  midtopview:{
+  	width:100
+  },
+  midtop:{
+  	fontSize:30,
+  	fontWeight:'bold'
+  },
+  topview:{
+  	justifyContent:'center',
+  	alignItems: 'center',
+  	width:60,
+  	height:50
+  },
+  toptext: {
+  	fontFamily: 'sans-serif',
+  	color: 'white'
+  },
+  circle: {
+    borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+    width: Dimensions.get('window').width * 0.4,
+    height: Dimensions.get('window').width * 0.4,
+    backgroundColor:'#ddd',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 4,
+    borderColor: '#FFF'
+  },
+  top: {
+  	position: 'absolute',
+	height: 245,
+	left: -1,
+	right: 0,
+	top: 0,
+	backgroundColor: '#5DB075'
+  },
+});
---
+++ ExpensesBarFinal.tsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View, TouchableHighlight, Dimensions } from 'react-native';
+
+export default function ExpensesBarFinal() {
+  return (
+  	<View style={{backgroundColor:'#F9F9FB', flex:1}}>
+	    <View style={{top: 125, alignItems:'center', justifyContent: 'center'}}>
+        <Text style={{fontSize:22, fontWeight: 'bold'}}>
+          Insights
+        </Text>
+      </View> 
+	    <View style={{top: 150, alignItems:'center', justifyContent: 'center'}}>
+        <BarChart
+          style={graphStyle}
+          data={data}
+          width={screenWidth}
+          height={220}
+          yAxisLabel="$"
+          chartConfig={chartConfig}
+          verticalLabelRotation={30}
+        />
+      </View>
+      <View style={{top:175, left:40}}>
+        <Text style={{fontSize:18, fontWeight:'bold', color:'#1C1939'}}>
+          Expenses
+        </Text>
+      </View>
+      <View style={{top:200, left:40, flex:1, flexDirection:'row', justifyContent:'flex-start'}}>
+        <View>
+          <Image style={{width:41, height:46, borderRadius:16}} source={require('./water.png')}></Image>
+        </View>
+        <View style={{left:20}}>
+          <Text style={{fontWeight:'bold'}}>
+            Item 1
+          </Text>
+          <View style={{right:20}}>
+          <Text style={{fontWeight:'bold'}}>
+            20%
+          </Text>
+        </View>
+      </View>
+      <View style={{top:125, left:40, flex:1, flexDirection:'row', justifyContent:'flex-start'}}>
+        <View>
+          <Image style={{width:41, height:46, borderRadius:16}} source={require('./harvest.png')}></Image>
+          </View>
+        <View style={{left:20}}>
+          <Text style={{fontWeight:'bold'}}>
+            Item 2
+          </Text>
+          <View style={{right:20}}>
+          <Text style={{fontWeight:'bold'}}>
+            15%
+          </Text>
+        </View>
+      </View>
+      <View style={{top:50, left:40, flex:1, flexDirection:'row', justifyContent:'flex-start'}}>
+        <View>
+          <Image style={{width:41, height:46, borderRadius:16}} source={require('./trade.png')}></Image>
+          </View>
+        <View style={{left:20}}>
+          <Text style={{fontWeight:'bold'}}>
+            Item 3
+          </Text>
+          <View style={{right:20}}>
+          <Text style={{fontWeight:'bold'}}>
+            15%
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const data = {
+  labels: ["Item 1", "Item 2", "Item 3"],
+  datasets: [
+    {
+      data: [20, 45, 28]
+    }
+  ]
+}
+
+const styles = StyleSheet.create({
+  left:{
+  	position:'absolute',
+  	left: 17
+  },
+  right:{
+  	position:'absolute',
+  	right: 14
+  },
+  midtopview:{
+  	width:100
+  },
+  midtop:{
+  	fontSize:30,
+  	fontWeight:'bold'
+  },
+  topview:{
+  	justifyContent:'center',
+  	alignItems: 'center',
+  	width:60,
+  	height:50
+  },
+  toptext: {
+  	fontFamily: 'sans-serif',
+  	color: 'white'
+  },
+  circle: {
+    borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+    width: Dimensions.get('window').width * 0.4,
+    height: Dimensions.get('window').width * 0.4,
+    backgroundColor:'#ddd',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 4,
+    borderColor: '#FFF'
+  },
+  top: {
+  	position: 'absolute',
+	height: 245,
+	left: -1,
+	right: 0,
+	top: 0,
+	backgroundColor: '#5DB075'
+  },
+});
