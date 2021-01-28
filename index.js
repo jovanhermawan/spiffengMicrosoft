0b3d2fe49b8ee512bdbdcfc99414be6df97f26fc
Jovan Hermawan <49465450+jovanhermawan@users.noreply.github.com>
2021-01-28 21:39:41 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 import Page from './Page';
+import DietBarFinal from './DietBarFinal';
+import ExpensesBarFinal from './ExpensesBarFinal';
+import ProgressBarFinal from './ProgressBarFinal';
+import Welcome from './Welcome';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Page);
+registerRootComponent(ExpensesBarFinal);
 
