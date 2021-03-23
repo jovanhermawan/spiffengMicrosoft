11b9b38eb00d35b8682854ed8c291e86521291a2
darisdzakwanhoesien2 <48860799+darisdzakwanhoesien2@users.noreply.github.com>
2021-03-23 13:14:50 +0800

    Add files via upload
    PowerBI integration with ReactJS

---
+++ powerbi-spiffeng/File.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export class ReactCircleCard extends React.Component<{}>{
+    render(){
+        return (
+            <div className="circleCard">
+                Hello, React!
+            </div>
+        )
+    }
+}
+
+export default ReactCircleCard;
